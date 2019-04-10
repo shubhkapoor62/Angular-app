a60222eff5f3f7356a39205ef13601d9e9d16368
shubhkapoor62 <shubhkapoor62@gmail.com>
2019-04-10 20:52:19 +0530

    Receipe & shopping List Components detailed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test',
+     'https://www.maxpixel.net/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg')
+  ];
+  constructor() {
+    this.recipes.push(new Recipe('A test Recipe', 'This is simply a test',
+    'https://www.maxpixel.net/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'));
+  }
 
   ngOnInit() {
   }
